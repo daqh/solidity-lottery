40424b7328dfe6a0ec29ac83fe2387d456e3d2f2
YuriBrandi <destroyercrazy0@gmail.com>
2024-08-20 01:40:23 +0200

    Added psudo-randomness to Lottery
    - fixed missing public properties.
    - removed extraction manager requirement.
    - Added pseudo-randomness with dynamic seed based on the Commit-Reveal Scheme.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.0;
 contract Lottery {
 
     address public manager;
+    string public name;
+    uint256 public expiration;
+    uint256 public prize;
+
     address[] public contestants;
-    string name;
-    uint256 expiration;
-    uint256 prize;
-    bool ended;
+    mapping (address => bytes32) public commitments;
+    uint256 public dynamicSeed;
+
+    address public winner;
+
 
     constructor(address _manager, string memory _name, uint256 _expiration, uint256 _prize) {
         manager = _manager;
@@ -45,14 +50,20 @@ contract Lottery {
         return prize;
     }
 
-    //Access Restriction pattern to owner and expiration time.
-    modifier onlyManager() {
-        require(msg.sender == manager);
-        _;
-    }
+    //Access Restriction pattern to expiration time.
+
+    /*
+        Removed manager restriction so that the extraction
+        can be performed by anyone, avoiding possible frauds.
+    */
 
     modifier onlyAfterExpiration() {
         require(block.timestamp >= expiration);
+        _;
+    }
+
+    modifier onlyAfterRevealInterval() {
+        require(block.timestamp >= expiration + 1 days);
         _;
     }
 
@@ -62,24 +73,44 @@ contract Lottery {
     }
 
     modifier onlyIfNotEnded() {
-        require(!ended);
+        require(winner == address(0));
         _;
     }
 
-    function enter() public payable onlyBeforeExpiration {
+    /*
+        Provide de-centralized, low-cost, unpredictable randomness
+        by Commit-Reveal Scheme.
+
+        It is not exploitable as a contestant has to choose a number before
+        knowing other contestants' chosen values.
+
+    */
+
+    function enter(uint256 number) public payable onlyBeforeExpiration {
         require(msg.value >= .01 ether);
-        contestants.push(msg.sender);
+        require(commitments[msg.sender] == bytes32(0), "Address already entered");
+
+        bytes32 commitment = keccak256(abi.encode(msg.sender, number));
+        commitments[msg.sender] = commitment;
     }
 
+    function reveal(uint256 number) public onlyAfterExpiration onlyIfNotEnded{
+        // Recreate the commitment and check if it matches the stored commitment
+        bytes32 commitment = keccak256(abi.encode(msg.sender, number));
+        require(commitment == commitments[msg.sender], "Incorrect number for reveal commitment");
 
-    function extractWinner() public onlyManager onlyAfterExpiration {
-        //temporarily always extracts first partecipant
-        address winner = contestants[0];
-        ended = true;
+        contestants.push(msg.sender);
+        //Increase Dynamic Seed with overflow protection.
+        dynamicSeed = (dynamicSeed + number) % type(int256).max;
+    }
+
+    function extractWinner() public onlyAfterRevealInterval onlyIfNotEnded{
+        require(contestants.length > 0, "No participant revealed their number");
+
+        winner = contestants[uint256(dynamicSeed) % contestants.length];
 
         //Verify balance
         require(address(this).balance >= prize, "Insufficient balance");
-
         payable(winner).transfer(prize);
     }
 }
