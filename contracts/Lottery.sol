c27fc90f66e2595bb66131323fc541510055a17b
YuriBrandi <destroyercrazy0@gmail.com>
2024-08-19 19:55:45 +0200

    Lottery contract improvements
    - deleted Ticket.sol
    - Added lottery name, expiration, prize and flag for extraction.
    - Added access restrictions.
    - Temporary winner extraction.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,36 +1,85 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
-import "./Ticket.sol";
 
 contract Lottery {
 
     address public manager;
-    Ticket[] public tickets;
+    address[] public contestants;
+    string name;
+    uint256 expiration;
+    uint256 prize;
+    bool ended;
 
-    constructor(address _manager) {
+    constructor(address _manager, string memory _name, uint256 _expiration, uint256 _prize) {
         manager = _manager;
+        name = _name;
+        expiration = _expiration;
+        prize = _prize;
     }
 
     receive() external payable {}
 
     fallback() external payable {}
 
-    function enter() public payable {
-        require(msg.value >= .01 ether);
-        Ticket ticket = new Ticket(msg.sender);
-        tickets.push(ticket);
-    }
-
-    function getTickets() public view returns (Ticket[] memory) {
-        return tickets;
+    function getContestants() public view returns (address[] memory) {
+        return contestants;
     }
 
     function getOwner() public view returns (address) {
         return manager;
     }
 
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function isLotteryOngoing() public view returns (bool) {
+        return block.timestamp < expiration;
+    }
+
+    function getPrize() public view returns (uint256) {
+        return prize;
+    }
+
+    //Access Restriction pattern to owner and expiration time.
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    modifier onlyAfterExpiration() {
+        require(block.timestamp >= expiration);
+        _;
+    }
+
+    modifier onlyBeforeExpiration() {
+        require(block.timestamp < expiration);
+        _;
+    }
+
+    modifier onlyIfNotEnded() {
+        require(!ended);
+        _;
+    }
+
+    function enter() public payable onlyBeforeExpiration {
+        require(msg.value >= .01 ether);
+        contestants.push(msg.sender);
+    }
+
+
+    function extractWinner() public onlyManager onlyAfterExpiration {
+        //temporarily always extracts first partecipant
+        address winner = contestants[0];
+        ended = true;
+
+        //Verify balance
+        require(address(this).balance >= prize, "Insufficient balance");
+
+        payable(winner).transfer(prize);
+    }
 }
